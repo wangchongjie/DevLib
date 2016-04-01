861e6beaf16bc043eb1acae931ee69a3fd175aa0
wangchongjie <wangcj519@gmail.com>
2016-04-01 11:22:48 +0800

    package update

--- src/test/java/com/baidu/unbiz/devlib/CollectionTest.java
+++ src/test/java/com/baidu/unbiz/devlib/CollectionTest.java
@@ -1,5 +1,6 @@
 package com.baidu.unbiz.devlib;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -7,12 +8,13 @@ import java.util.Set;
 import org.junit.Test;
 import org.springframework.util.Assert;
 
+import com.baidu.unbiz.devlib.collection.IdCollector;
 import com.baidu.unbiz.devlib.collection.ToKvMapConverter;
 import com.baidu.unbiz.devlib.collection.ToMapConverter;
 import com.baidu.unbiz.devlib.collection.ToSetMapConverter;
 import com.baidu.unbiz.devlib.vo.ViewItem;
 
-public class ToolboxCollectionTest {
+public class CollectionTest {
 
     @Test
     public void testToKvMapConverterTest() {
@@ -64,6 +66,35 @@ public class ToolboxCollectionTest {
 
         Assert.isTrue(itemSetMap.size() > 1);
         System.out.print(itemSetMap);
+    }
+
+    @Test
+    public void testIdCollectorTest() {
+        List<ViewItem> list = ViewItem.mockList1();
+        IdCollector collector = new IdCollector<ViewItem>() {
+        };
+        Set<Integer> ids = collector.collectIds(list, "id");
+        Set<String> names = collector.collectIds(list, "name");
+
+        Assert.isTrue(ids.size() > 1);
+        System.out.print(ids);
+        Assert.isTrue(names.size() > 1);
+        System.out.print(names);
+    }
+
+    @Test
+    public void testElseIdCollectorTest() {
+        List<ViewItem> list = ViewItem.mockList1();
+        Set<Integer> idSet = new HashSet<Integer>();
+        idSet.add(1);
+        idSet.add(2);
+
+        IdCollector collector = new IdCollector<ViewItem>() {
+        };
+        Assert.isTrue(collector.containIds(list, "id", idSet));
+
+        collector.removeItemByIds(list, "id", idSet);
+        Assert.isTrue(!collector.containIds(list, "id", idSet));
     }
 
 }
